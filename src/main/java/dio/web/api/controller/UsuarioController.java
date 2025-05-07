a40032e63ace0e35ac616416ae36bb589d514e5c
periclesevt <periclesevt@gmail.com>
2025-05-07 15:53:56 -0300

    fixing UsuarioController

--- src/main/java/dio/web/api/controller/UsuarioController.java
+++ src/main/java/dio/web/api/controller/UsuarioController.java
@@ -3,30 +3,34 @@ package dio.web.api.controller;
 import dio.web.api.model.Usuario;
 import dio.web.api.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UsuarioController {
     @Autowired
     private UsuarioRepository repository;
 
-    @GetMapping("/users")
+    @GetMapping()
     public List<Usuario> getUsers(){
         return repository.findAll();
     }
 
-    @GetMapping("/users/{username}")
+    @GetMapping("/{username}")
     public Usuario getOne (@PathVariable("username")String username){
         return repository.findByUsername(username);
     }
 
-    @DeleteMapping("/users/{id}")
+    @DeleteMapping("/{id}")
     public void delete(@PathVariable("id") Integer id){
         repository.deleteById(id);
+    }
+
+
+    @PostMapping()
+    public void postUser(@RequestBody Usuario usuario){
+        repository.save(usuario);
     }
 }
